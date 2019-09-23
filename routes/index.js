a54f0fa465fc2d4494019d790ac1e8f683924e3d
Nikhil BN <nikhilbnaradhya1.gem@gmail.com>
2019-09-23 14:15:48 +0530

    Register for events

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const authRouter = require('./auth');
 const userRouter = require('./users');
+const eventsRouter = require('./events');
 const express = require('express');
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
+router.use('/events', eventsRouter);
 
 module.exports = router;
